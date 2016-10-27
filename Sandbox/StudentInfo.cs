fa22adc9d2f95fbd1879de7766f8a441d546cad1
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-10-27 13:14:13 +0200

    trouble at Getaverage and getaverageforallstudents.

--- Sandbox/StudentInfo.cs
+++ Sandbox/StudentInfo.cs
@@ -17,39 +17,63 @@ namespace Sandbox
         // Return the number of students in the group of students
         public int GetStudentCount()
         {
-            // The below must be changed
-            return 0;
+            return students.Count;
         }
 
         // Add a single student to the group of students
         public void AddStudent(int id, Student aStudent)
         {
-            // Add code here
+            students.Add(id, aStudent);
         }
 
         // Given an id, return the student with that id.
         // If no student exists with the given id, return null
         public Student GetStudent(int id)
         {
-            // The below must be changed
-            return null;
+            if (students.ContainsKey(id))
+            {
+                return students[id];
+
+            }
+            else
+            {
+                return null;
+            }
         }
 
         // Given an id, return the score average for the student with that id.
         // If no student exists with the given id, return 0 (zero).
-        public int GetAverageForStudent(int id)
+        public int GetAverageForStudent(int id) // SOLVE MOFO!!
         {
-            // The below must be changed
-            return 0;
+            if (students.ContainsKey(id))
+            {
+                int studentAverage = 0;
+                studentAverage = 0 + 0;
+
+                return studentAverage;
+            }
+            else
+            {
+                return 0;
+            }
+            
         }
 
         // Calculate the total test score average for ALL students
         // in the group of students.
         // TIP: Use the method GetAllStudentId and a loop...
-        public int GetTotalAverage()
+        public int GetTotalAverage()  // SOLVE IT MOFO!
         {
-            // The below must be changed
+            foreach (var item in students)
+            {
+                int Studentavg = 0;
+                Studentavg = 2 / 2;
+                return Studentavg;
+                
+
+            }
             return 0;
+            
         }
 
         // Returns a list of all ids of the students in the group of students
